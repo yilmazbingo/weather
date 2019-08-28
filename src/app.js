6d02f0a48ac4018da5f70421d8e91620315b5c0e
omar ibi <omaribi@MacBook-Air.local>
2019-08-27 20:51:04 -0400

    modified

---
+++ src/app.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
+const geocode = require("./utils/geocode.js");
+const forecast = require("./utils/forecast");
+
+//set up static directory
+app.use(express.static(path.join(__dirname, "../public")));
+
+//config for  handlebars
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "../templates/views"));
+hbs.registerPartials(path.join(__dirname, "../templates/partials"));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Weather App", name: "yilmaz" });
+});
+
+// app.get("/products", (req, res) => {
+//   if (!req.query.search) {
+//     return res.send({ error: "you must provide search term" });
+//   }
+//   res.send({ products: [] });
+// });
+app.get("/help", (req, res) => {
+  res.render("help");
+});
+
+// app.get("/weather", async (req, res) => {
+//   if (!req.query.address) {
+//     return res.send({ error: "provide address" });
+//   }
+//   const result = geocode(
+//     req.query.address,
+//     (error, { latitude, longitude, location }) => {
+//       if (error) {
+//         return res.send({ error });
+//       }
+//       forecast(latitude, longitude, (error, forecastData) => {
+//         if (error) {
+//           return res.send({ error });
+//         }
+//         res.send({
+//           forecast: forecastData,
+//           location,
+//           address: request.query.address
+//         });
+//       });
+//     }
+//   );
+// });
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address!"
+    });
+  }
+
+  geocode(req.query.address, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return res.send({ error });
+    }
+
+    forecast(latitude, longitude, (error, forecastData) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      res.send({
+        forecast: forecastData,
+        location,
+        address: req.query.address
+      });
+    });
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", { 404: "Help article not found" });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", { errorMessage: "404 Page Not Found " });
+});
+
+app.listen(5000, () => {
+  console.log("listening");
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,23 @@
+const request = require("request");
+const forecast = (lat, long, callback) => {
+  const url = `https://api.darksky.net/forecast/8357062dc1df35714c75ea3a19ccf89b/${lat},${long}`;
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback("Unable to connect to weather service!", undefined);
+    } else if (body.error) {
+      callback("Unable to find location", undefined);
+    } else {
+      callback(
+        undefined,
+        body.daily.data[0].summary +
+          " It is currently " +
+          body.currently.temperature +
+          " degress out. There is a " +
+          body.currently.precipProbability +
+          "% chance of rain."
+      );
+    }
+  });
+};
+
+module.exports = forecast;
